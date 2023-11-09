ee0091b4b283c4676d86cb95356d62a851b8ab0b
ugniusu <ugniususc@gmail.com>
2023-11-09 11:34:15 +0200

    papildymas

--- script.js
+++ script.js
@@ -30,26 +30,26 @@
 //   `Viena karta gyveno ${vardas}, kurio pavarde buvo ${pavarde} ir jis buvo gimes ${kitasAmzius}`
 // );
 
-const ugniusAmzius = 1997;
-const kitasAmzius = 2014;
-const isYounger = true;
+// const ugniusAmzius = 1997;
+// const kitasAmzius = 2014;
+// const isYounger = true;
 
-if (ugniusAmzius < kitasAmzius) {
-  console.log(isYounger);
-} else {
-  console.log("Tu malagis");
-}
+// if (ugniusAmzius < kitasAmzius) {
+//   console.log(isYounger);
+// } else {
+//   console.log("Tu malagis");
+// }
 
-const vardenis = "ugnius uscilas";
-const index = 8;
-console.log(`The character at index ${index} is ${vardenis.charAt(index)}`);
+// const vardenis = "ugnius uscilas";
+// const index = 8;
+// console.log(`The character at index ${index} is ${vardenis.charAt(index)}`);
 
-////////////////////////////////
-const daug1 = ["a", "b", "c"];
-const daug2 = ["belekoks", "belekaip", "belekur"];
-const daug3 = daug2.concat(daug1);
+// ////////////////////////////////
+// const daug1 = ["a", "b", "c"];
+// const daug2 = ["belekoks", "belekaip", "belekur"];
+// const daug3 = daug2.concat(daug1);
 
-console.log(daug3);
+// console.log(daug3);
 
 //////////////////////////////
 // const tekstas = "Mano vardas Jonas is pavarde Jonaitis ir as gyvenu Kaune";
@@ -182,3 +182,134 @@ console.log(daug3);
 // console.log(tekstas.substring(0, 6));
 // console.log(tekstas.substring(6));
 // console.log(tekstas.substring(6, 3));
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// const tekstas = "Ugnius Uscilas"
+// const str = "Petras Petraitis"
+// const together = tekstas.concat(str);
+// console.log(together);
+
+// let text1 = "                 Laba diena.            swx all"
+// let text2 = "Kaip laikotes?"
+// let text3 = "Paaukokit pinigeli prasau"
+
+// let concatedText = text1.concat(text2, text3.charAt(5));
+// console.log(concatedText);
+
+// let concatedText = text1.endsWith(".");
+// console.log(concatedText);
+
+// let concatedText = text1.indexOf("Kaip");
+// console.log(concatedText);
+
+// let concatedText = text1.length
+// console.log(concatedText);
+
+// let concatedText = text1.match("diena")
+// console.log(concatedText);
+
+// let concatedText = text1.repeat(3)
+// console.log(concatedText);
+
+// let concatedText = text1.replace("visi", "vakaras")
+// console.log(concatedText);
+
+// let concatedText = text1.search("visi")
+// console.log(concatedText);
+
+// let concatedText = text1.slice("visi")
+// console.log(concatedText);
+
+// let concatedText = text1.split(",")
+// console.log(concatedText);
+
+// let concatedText = text1.substring(5,9)
+// let toUpper = text1.charAt(5).toUpperCase()
+// let subs = text1.substring(6, 10);
+// let concated = toUpper.concat(subs)
+// let text = text1.replace("diena", concated);
+// let changed = text1.replace("d", "D");
+// console.log(concatedText);
+// console.log(toUpper);
+// console.log(subs);
+// console.log(concated);
+// console.log(text);
+// console.log(changed);
+
+// let concatedText = text1.toString()
+// console.log(concatedText);
+
+// let concatedText = text1.trim()
+
+// console.log(concatedText);
+// console.log(text1);
+
+// let x = 100 / "Apple";
+// isNaN(x);
+// console.log(isNaN(x));
+
+// let x = 10;
+// console.log(x++);
+// console.log(x);
+
+// let y = 9;
+// console.log(y--);
+// console.log(--y);
+// console.log(y++
+//   );
+
+let str = "Ugnius Uscilas"
+str = "Petras Petraitis"
+console.log(str);
+
+let numeris = 5;
+console.log(numeris++);
+console.log(numeris++);
+console.log(numeris);
+
+let number = 10;
+number = 7 * number;
+console.log(number);
+
+let number2 = 5
+console.log(--number2);
+console.log(--number2);
+console.log(--number2);
+console.log(--number2);
+
+let number3 = "100.99"
+console.log(typeof +"100");
+console.log(typeof parseInt(number3, 2))
+console.log(typeof Number(number3))
+
+
+
+
+let ugniusHeight = 1.95;
+let ugniusMass = 101;
+let ugniusKMI = ugniusMass / (ugniusHeight**2);
+console.log(ugniusKMI);
+
+let petrasHeight = 1.60;
+let petrasMass = 75;
+let petrasKMI = petrasMass / (petrasHeight**2);
+console.log(petrasKMI);
+
+if (ugniusKMI > petrasKMI) {
+  console.log(`Ugniaus KMI yra ${ugniusKMI} ir yra didesnis negu Petro KMI: ${petrasKMI} `)
+} else {
+  console.log(`Ugniaus KMI yra ${ugniusKMI} ir yra mazesnis negu Petro KMI: ${petrasKMI} `)
+}
+
+let a = 8;
+let b = 6;
+let c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+console.log(c);
+
+let celc = 35;
+let far = (celc * 9/5) + 32;
+console.log(far);
